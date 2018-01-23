412ae25e4411a69944ece9451c6cf6747b95ec0d
Rienshaliu <rienshaliu@gmail.com>
2018-01-24 01:05:53 +0800

    beautifulsoup

--- ig.api/instagram_crawler.py
+++ ig.api/instagram_crawler.py
@@ -1,2 +1,9 @@
-import urllib2
+import urllib.request
 from bs4 import BeautifulSoup
+
+tag = "cal_foodie"
+
+quote_page = "https://www.instagram.com/%s/"%tag
+page = urllib.request.urlopen(quote_page)
+soup = BeautifulSoup(page, 'html.parser')
+print(soup.prettify()[0:100])
